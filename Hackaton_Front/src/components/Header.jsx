8304a2ee2071f6fc7a13fff313bf29a989355f32
AnaAguileraMorales88 <anitaaguilera@hotmail.com>
2025-10-15 17:01:34 +0200

    Cambios de estilo en ranking.jsx

--- Hackaton_Front/src/components/Header.jsx
+++ Hackaton_Front/src/components/Header.jsx
@@ -1,9 +1,12 @@
     import React from "react";
+        import { Link } from "react-router-dom";
+
+
 
     function Header() {
     return (
         <header className="flex items-center justify-center py-6 bg-gray-50">
-        <img src="src/assets/logo.png" alt="Logo del proyecto" className="h-24 w-24 sm:h-32 sm:w-32"/>
+        <Link to="/"><img src="src/assets/logo.png" alt="Logo del proyecto" className="h-24 w-24 sm:h-32 sm:w-32"/></Link>
         <h1 className="text-5xl sm:text-6xl font-bold text-black-800">Ciudad Viva BCN</h1>
         </header>
     );

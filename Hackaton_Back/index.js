c1d24639cfdcc87261c9e8984d3f57168d1ee675
sergidl <sergidiazlopez3@gmail.com>
2025-10-15 11:49:02 +0200

    draft puntos de referencia

--- Hackaton_Back/index.js
+++ Hackaton_Back/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import fetchData from './puntosDeInteres.js';
 
 const app = express();
 const PORT = 3000;
@@ -12,5 +13,11 @@ app.listen(PORT, () => {
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send(fetchData());
 });
+
+// If you're on Node < 18, install node-fetch:
+// npm install node-fetch
+// and then: import fetch from 'node-fetch';
+
+

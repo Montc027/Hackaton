59cdb6612e84bf8d2901f71892d21b8d37808516
Mio <gusw.0246.lpsyg@gmail.com>
2025-10-15 16:50:49 +0200

    mapa conectada con json

--- Hackaton_Front/src/App.jsx
+++ Hackaton_Front/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import Map from "./pages/Map";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/mapa" element={<Map /> } />
       </Routes>
       <Footer />
     </Router>

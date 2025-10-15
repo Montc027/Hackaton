f8f98218ec2e2dc649180199d846908251aa6a86
Mio <gusw.0246.lpsyg@gmail.com>
2025-10-15 13:46:30 +0200

    agregado token de map

--- Hackaton_Front/src/components/Map.jsx
+++ Hackaton_Front/src/components/Map.jsx
@@ -7,9 +7,24 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 function Map() {
   const mapRef = useRef()
   const mapContainerRef = useRef()
+
+    useEffect(() => {
+    const apiKey = import.meta.env.VITE_mapToken;
+    mapboxgl.accessToken = apiKey;
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+    });
+
+    return () => {
+      mapRef.current.remove()
+    }
+  }, [])
+
+
+
   return (
     <>
-       <div id='map-container' className='h-full w-full' ref={mapContainerRef}/>
+       <div id='map-container' className='h-[500px] w-[700px]' ref={mapContainerRef}/>
     </>
   )
 }
